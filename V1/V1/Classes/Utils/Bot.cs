087011ade97a69076454ed33379d1a0ad76909bd
Maarten Peels <maartenpeels1012@hotmail.com>
2015-09-25 11:21:12 +0200

    Created Solution

---
+++ V1/V1/Classes/Utils/Bot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V1.Classes.Utils
+{
+    public static class Bot
+    {
+        //Send status
+        //Keep version
+        //Bot name etc
+    }
+}
---
+++ V1/V1/main.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace V1
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
